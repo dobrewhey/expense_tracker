ed5ab34fbb9de9d0bea30d2f3306c8244a3dea58
dobrewhey <fiejtekmichal@gmail.com>
2024-01-02 13:27:36 +0100

    AppBar init

---
+++ expense_tracker/lib/widgets/expenses.dart
@@ -0,0 +1,47 @@
+import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
+import 'package:expense_tracker/models/expense.dart';
+import 'package:flutter/material.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<Expenses> createState() {
+    return _ExpensesState();
+  }
+}
+
+class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      amount: 19.99,
+      date: DateTime.now(),
+      title: 'Flutter Course',
+      category: Category.work,
+    ),
+    Expense(
+      amount: 15.69,
+      date: DateTime.now(),
+      title: 'Cinema',
+      category: Category.leisure,
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter ExpenseTracker'),
+        actions: [
+          IconButton(onPressed: () {}, icon: const Icon(Icons.add)),
+        ],
+      ),
+      body: Column(
+        children: [
+          const Text('The chart'),
+          Expanded(child: ExpensesList(expenses: _registeredExpenses))
+        ],
+      ),
+    );
+  }
+}
